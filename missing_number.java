98c569da1f41ba4dd233f306b226ce222259a383
Shubhanshu9696 <92735584+Shubhanshu9696@users.noreply.github.com>
2022-06-19 02:38:58 +0530

    Update missing_number.java

--- missing_number.java
+++ missing_number.java
@@ -1,4 +1,19 @@
+/*Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 
+Example 1:
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 2:
+Input: nums = [0,1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 3:
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.*/
 
 import java.util.Scanner;
 import java.util.HashMap;
@@ -15,8 +30,8 @@ public class missing_number {
             ans = i;
         }
         return ans ;
-
     }
+    
     public static void main(String[] args ){
         Scanner sc = new Scanner( System.in);
         System.out.println("enter the size of array");
@@ -29,3 +44,31 @@ public class missing_number {
         System.out.println(ans);
     }    
 }
+
+
+                    //faster approcah
+
+// import java.util.Scanner;
+// import java.util.HashMap;
+
+// public class missing_number {
+
+//     static int findNumber(int[] arr){
+//         int sum = (nums.length)*(nums.length+1)/2;
+//         for (int i : nums)
+//             sum = sum-i;
+//         return sum;
+//     }
+    
+//     public static void main(String[] args ){
+//         Scanner sc = new Scanner( System.in);
+//         System.out.println("enter the size of array");
+//         int n = sc.nextInt();
+//         int [] arr = new int[n];
+//         System.out.println("enter the element of array");
+//         for(int i = 0 ; i<n ; i++)
+//             arr[i] = sc.nextInt() ; 
+//         int ans  = findNumber(arr);
+//         System.out.println(ans);
+//     }    
+// }
